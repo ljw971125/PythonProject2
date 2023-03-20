9b16ec54d8ad6b9dc7c1c509ed5dfe34ba7463d1
ljw971125 <ljw971125@naver.com>
2023-03-20 10:27:19 +0900

    프로젝트(틀잡기)

--- code/ProjectMain.py
+++ code/ProjectMain.py
@@ -1,6 +1,6 @@
 from tkinter import *
-import tkinter # 인터페이스를 만들 때 사용합니다.
-import csv # csv 파일을 불러올 때 사용합니다.
+import tkinter # 인터페이스를 만들 때
+import csv # csv 파일을 불러올 때 사용합니다
 import os 
 
 PATH = os.path.dirname(os.path.realpath(__file__))
@@ -10,20 +10,34 @@ f = open('서울시 구별건.csv')
 reader = csv.reader(f)
 data = [row[1] for row in reader]
 
-root = Tk()
+window = tkinter.Tk()
+window.title("지역별 교통사고 현황")
+window.geometry("300x200")
 
-frame = Frame(root) # 프레임 생성
-frame.pack(side=LEFT) # 프레임을 윈도우에 배치
+# 프레임 생성
+frame = tkinter.Frame(window)
+frame.pack(side=LEFT)
 
 scrollbar = Scrollbar(frame) # 스크롤바 생성
 scrollbar.pack(side=RIGHT, fill=Y) # 스크롤바를 프레임의 오른쪽에 붙임
-
 
 mylist = Listbox(frame, yscrollcommand=scrollbar.set, height=0) # 리스트바 생성, 스크롤바 연결, 높이 0으로 설정
 for data in data[3:]:
     mylist.insert(tkinter.END, data)
 mylist.pack(side=LEFT) # 리스트바를 프레임의 왼쪽에 붙임
 
-scrollbar.config(command=mylist.yview) # 스크롤바에 리스트바의 y축 이동을 연결
+# 레이블 생성
+label = tkinter.Label(window)
+label.pack()
 
-mainloop()
+# 콜백 함수 정의
+def show_info(event):
+    # 선택된 항목의 인덱스와 정보를 레이블에 표시
+    index = mylist.curselection()[0]
+    info = mylist.get(index)
+    label.config(text=f"선택한 지역: {info}")
+
+# 리스트박스에 콜백 함수 연결
+mylist.bind("<<ListboxSelect>>", show_info)
+
+window.mainloop()
